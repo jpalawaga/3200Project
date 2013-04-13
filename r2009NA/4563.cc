92f9fc6bedcb4bcecfb1377118a651849c093be1
Camara Lerner <camara.lerner@gmail.com>
2013-04-13 04:40:11 -0600

    trying to fix 4563

--- r2009NA/4563.cc
+++ r2009NA/4563.cc
@@ -10,15 +10,20 @@
 #include <iomanip>
 #include <string>
 #include <stack>
+#include <cstdio>
 using namespace std;
 
-bool hexNum(const string &a, int &index) {
+/****
+     a should only be everything after the x
+****/
+bool hexNum(const string &a, string::size_type &index) {
+   //cout << a << endl;
    string::size_type j = 0;
-   for(string::size_type i = 0; i < a.size(); ) {
-      if((a[i] >= '0' && a[i] <= '9') || (a[i] >= 'a' && a[i] <= 'f')
+   for(string::size_type i = 0; i < a.size(); ++i) {
+      if(   (a[i] >= '0' && a[i] <= '9')
+	 || (a[i] >= 'a' && a[i] <= 'f')
 	 || (a[i] >= 'A' && a[i] <= 'F')) {
-	 j = i;
-	 ++i;
+	 ++j;
       } else {
 	 break;
       }
@@ -31,8 +36,10 @@ bool hexNum(const string &a, int &index) {
    }
 }
 
-bool handleand(const string &a, int &index) {
+// holds the info after the < and > symbols
+bool handleand(const string &a, string::size_type &index) {
    if(a.empty()) return false;
+   //cout << a << endl;
    if(a.substr(0, 2) == "lt" || a.substr() == "gt") {
       index += 2;
       return true;
@@ -40,24 +47,82 @@ bool handleand(const string &a, int &index) {
       index += 3;
       return true;
    } else if(a[0] == 'x') {
+      ++index;//eat the x
       if(a.size() <= 1) return false;
-      return hexNum(a.substr(1, a.size() - 1), index);
+      return hexNum(a.substr(1, a.size() - 1), index);//start past the x
    } else {
       return false;
    }
 }
 
-bool handlelt(const string &a, int &index) {
+bool validTag(const string &a) {
+   for(string::size_type i = 0; i < a.size(); ++i) {
+      if(!islower(a[i]) && !isdigit(a[i])) return false;
+   }
+   return true;
+}
+
+// a holds the info between the < and > symbols
+bool handlelt(const string &a, stack<string> &context) {
    if(a.empty()) return false;
+   cout << a << endl;
+   if(a[0] == '/') {
+      if(context.empty()) return false;
+      if(a.substr(1, a.size() - 1) == context.top()) {
+	 context.pop();
+	 return true;
+      } else {
+	 return false;
+      }
+   } else if(a[a.size() - 1] == '/') {
+      return validTag(a.substr(0, a.size() - 1));
+   } else {
+      if(validTag(a)) {
+	 context.push(a);
+	 return true;
+      } else {
+	 return false;
+      }
+   }
    
 }
 
 int main() {
-
+   stack<string> context;
    string line;
-   while(getline(cin, line)) {
-
-
+   bool valid = true;
+   while(getline(cin, line) && cin.good()) {
+      for(string::size_type i = 0; i < line.size(); ) {
+	 if(line[i] == '&') {
+	    valid = handleand(line.substr(i + 1, line.size() - i - 1), i);
+	 } else if(line[i] == '<') {
+	    string::size_type index = line.find('>', i);
+	    if(index == line.size()) valid = false;
+	    else {
+	       valid = handlelt(line.substr(i + 1, index - 1 - i),
+				context);
+	       i = index + 1;
+	    }
+	 } else if(line[i] == '>') {
+	    valid = false;
+	 } else {
+	    valid = (line[i] >= 32) && (line[i] <= 127);
+	    ++i;
+	 }
+	 if(!valid) {
+	    break;
+	 }
+      }
+      if(valid) {
+	 if(context.empty()) {
+	    cout << "valid" << endl;
+	 } else {
+	    cout << "invalid" << endl;
+	 }
+      } else {
+	 cout << "invalid" << endl;
+      }
+	 
    }
    
    return 0;
