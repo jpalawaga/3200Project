e3569d080a9ce77b57857636c2ad5d123c0b31b7
Camara Lerner <camara.lerner@gmail.com>
2013-04-18 17:17:42 -0600

    have 4564 with correct answers and debugging code gone

--- r2009NA/4564.cc
+++ r2009NA/4564.cc
@@ -24,40 +24,24 @@ bool solvable(const int &start, const int &len) {
    } else if(len == 1) {
       return false;
    } else if(len == 2) {
-      //cout << a.substr(start, len) << endl;
-      //cout << "finished with " << (a[start] == a[start + 1]) << endl;
       return a[start] == a[start + 1];
    } else if(len == 3) {
-      //cout << a.substr(start, len) << endl;
-      //cout << "finished with "
-      //<< (a[start] == a[start + 1] && a[start + 1] == a[start + 2])
-      //<< endl;
       return (a[start] == a[start + 1]) && (a[start] == a[start + 2]);
    }
    if(a[start] == a[start + len - 1]) {
       // AxAyA
       int next = a.find(a[start], start + 1);
       while(next < start + len - 1) {
-	 //cout << a.substr(start, len) << endl;
-	 //cout << a.substr(start + 1, next - start - 1) << " "
-	 //    << a.substr(next + 1, len - next - 2) << endl;
-	 // cout << "AxAyA" << endl;
 	 if(solvable(start + 1, next - start - 1)) {
 	    if(solvable(next + 1, start + len - next - 2)) {
-
-	       //    cout << "finish with true" << endl;
 	       return (bool)(table[start][len] =
 			     true);
 	    }
 	 }
 	 next = a.find(a[start], next + 1);
-	 //cout << "next: " <<  next << endl;
       }
       // AxA
-      //cout << "AxA" << endl;
       if(solvable(start + 1, len - 2)) {
-	 //cout << a.substr(start, len) << endl;
-	 //cout << "finish with true" << endl;
 	 return (bool)(table[start][len] =
 		       true);
       }
@@ -65,18 +49,13 @@ bool solvable(const int &start, const int &len) {
 
    // loop through this another way
    for(int i = 1; i < len; ++i) {
-      // cout << "xy" << endl;
       if(solvable(start, i)) {
 	 if(solvable(start + i, len - i)) {
-	    //cout << a.substr(start, len) << endl;
-	    //cout << "finish with true" << endl;
 	    return (bool)(table[start][len] =
 			  true);
 	 }
       }
    }
-   //cout << a.substr(start, len) << endl;
-   //cout << "finish with false" << endl;
    return (bool)(table[start][len] = false);
 }
 
@@ -89,13 +68,9 @@ int main() {
 	 }
       }
       if(solvable(0, a.length())) {
-	 //cout << endl;
 	 cout << "solvable" << endl;
-	 //cout << endl;
       } else {
-	 //cout << endl;
 	 cout << "unsolvable" << endl;
-	 //cout << endl;
       }
       a.clear();
    }
